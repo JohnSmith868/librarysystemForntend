b4e7417430bb466c97d3c4140475cc0f8a3d9b88
johnsmith868 <john@john.com>
2020-05-25 12:12:37 +0800

    Updates

---
+++ precache-manifest.27034ab41fd88091879f4fdbdcfcd654.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ccc2627e2aa00f450df147b1f53d932",
+    "url": "/librarysystemForntend/index.html"
+  },
+  {
+    "revision": "99d7c6703141d37180d4",
+    "url": "/librarysystemForntend/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b08a117042747d004ccf",
+    "url": "/librarysystemForntend/static/css/main.b97f83fb.chunk.css"
+  },
+  {
+    "revision": "99d7c6703141d37180d4",
+    "url": "/librarysystemForntend/static/js/2.6711f302.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/librarysystemForntend/static/js/2.6711f302.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b08a117042747d004ccf",
+    "url": "/librarysystemForntend/static/js/main.592fb0ec.chunk.js"
+  },
+  {
+    "revision": "37b59fcacd487ad1ee93",
+    "url": "/librarysystemForntend/static/js/runtime-main.7daa90d5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/librarysystemForntend/static/media/logo.5d5d9eef.svg"
+  }
+]);
